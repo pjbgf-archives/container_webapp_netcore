ea80f94aeede7b00023a9d95bf6e8e4eddb5ad7d
Paulo Gomes <Paulo.Gomes.uk@gmail.com>
2017-11-24 05:12:00 +0000

    Add script to make it easier the process of setting up CircleCI variables.

---
+++ tools/circleci.sh
@@ -0,0 +1,83 @@
+# Script under MIT License
+# Original source from: https://raw.githubusercontent.com/pliyosan/create-tokens-variables-in-circle-ci/master/circleci.sh
+
+#!/bin/bash
+set -euo pipefail
+
+# Set all non-sensitive variables here:
+variables='DEPLOY_CI_RESOURCE_GROUP=ci-myapp-euw
+DEPLOY_CI_LOCATION=westeurope
+DEPLOY_CI_WEBAPP_NAME=ci-myapp
+DOCKER_REGISTRY_URI=https://YOUR-CR-NAME-GOES-HERE.azurecr.io
+DOCKER_IMAGE_NAME_WITH_REGISTRY=YOUR-CR-NAME-GOES-HERE.azurecr.io/sample/container-appservice'
+
+# For all sensitive variables below, call command line separately, this will help you not to commit secrets into your repo. :)
+#
+# DEPLOY_CI_SUBSCRIPTION_ID
+# DOCKER_REGISTRY_SERVER_USERNAME
+# DOCKER_REGISTRY_SERVER_PASSWORD
+# SERVICE_PRINCIPAL
+# SERVICE_PRINCIPAL_PASS
+# SERVICE_TENANT
+
+
+usage() { echo "Usage: $0 -t <circleCiToken> -a <githubAccountName> -p <projectName>" 1>&2; exit 1; }
+
+declare circleCiToken=""
+declare githubAccountName=""
+declare projectName=""
+
+# Initialize parameters specified from command line
+while getopts ":t:a:p:" arg; do
+	case "${arg}" in
+		t)
+			circleCiToken=${OPTARG}
+			;;
+		a)
+			githubAccountName=${OPTARG}
+			;;
+		p)
+			projectName=${OPTARG}
+			;;
+		esac
+done
+shift $((OPTIND-1))
+
+#Prompt for parameters if some required parameters are missing
+if [[ -z "$circleCiToken" ]]; then
+	echo "Circle CI Token:"
+	read circleCiToken
+	[[ "${circleCiToken:?}" ]]
+fi
+
+if [[ -z "$githubAccountName" ]]; then
+        echo "Github Account Name:"
+        read githubAccountName
+        [[ "${githubAccountName:?}" ]]
+fi
+
+if [[ -z "$projectName" ]]; then
+        echo "Project Name:"
+        read projectName
+        [[ "${projectName:?}" ]]
+fi
+
+
+posturl="https://circleci.com/api/v1.1/project/github/$githubAccountName/$projectName/envvar?circle-token=$circleCiToken"
+
+
+echo "Loading environment variables into project $githubAccountName/$projectName:"
+
+for variable in $variables;
+do
+    
+    set -- `echo $variable | tr '=' '\n'`
+    curl -X POST --header "Content-Type: application/json" -d "{\"name\":\"$1\", \"value\":\"$2\"}" $posturl
+
+done
+
+echo
+if [ $?  == 0 ];
+ then
+	echo "Environment variables loaded successfully."
+fi
